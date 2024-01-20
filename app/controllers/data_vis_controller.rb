23dc8412664d420f4050f2e298fa25526ccbba9e
jyin1003 <jyin1003@gmail.com>
2024-01-20 19:13:17 +1000

    added styling and budget utilisation

--- app/controllers/data_vis_controller.rb
+++ app/controllers/data_vis_controller.rb
@@ -2,7 +2,28 @@ class DataVisController < ApplicationController
     def data_view
         #overall tour information
         @tour_name = Tour.find_by(id: 7)&.name
-        @expenses = Expense.where(tour_id: 7)
-    end
+        @expenses_tour = Expense.where(tour_id: 7)
+        @expenses_all = Expense.all
 
+        #budget calculations
+        tour_budget = Tour.find_by(id: 1)&.budget 
+        total_expenses_cost = Expense.where(tour_id: 1).sum(:cost)
+        percentage_of_budget = (total_expenses_cost / tour_budget) * 100
+        puts "percentage budget: #{percentage_of_budget}"
+
+        @budget_chart = [
+            ['Expenses', total_expenses_cost.round(2) ],
+            ['Remaining Budget', (tour_budget - total_expenses_cost).round(2) ]
+        ]
+        @percentage_of_budget = percentage_of_budget.round(2)
+        
+        #benchmark averages
+        category_expenses = Expense.group(:category).average(:cost)
+
+        # Format data for Chartkick
+        @chart_data = category_expenses.map do |category, avg_cost|
+            { name: category.capitalize, data: avg_cost.round(2) }
+        
+        end
+    end
 end
