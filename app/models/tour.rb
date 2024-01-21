34462eed1bb191260cd3afacdfaaa127edf9a109
wwwww123412551 <ttth@mail.com>
2024-01-21 14:31:40 +1000

    Added linking and total price to tours page

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,3 +1,7 @@
 class Tour < ApplicationRecord
   has_many :expenses, dependent: :destroy
+
+  def total_cost
+    self.expenses.map(&:cost).sum
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   # Routes related to csv import for expenses 
   get "tours/:id/expenses" => "expenses#index", as: "tour_expenses"
   get "tours/:id/expenses/add" => "expenses#new"
-  get "tours/:id/expenses/import" => "expenses#show"
+  get "tours/:id/expenses/import" => "expenses#show", as: "expenses_import"
   post "tours/:id/expenses" => "expenses#create"
   
 
