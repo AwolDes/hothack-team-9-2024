da08c44608472913a6565b195cb55848ea6a7b4d
Tracey Nguyen <trace_n@hotmail.com>
2024-01-21 16:12:24 +1000

    add in the total cost for expense

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,6 +2,7 @@ class Tour < ApplicationRecord
   has_many :expenses, dependent: :destroy
 
   def total_cost
-    self.expenses.map(&:cost).sum
+    ap self.expenses
+    self.expenses.where.not(cost: nil).map(&:cost)&.sum
   end
 end
