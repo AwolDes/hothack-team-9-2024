09f4c56fc14beeed3116d06409f865af72ce5093
Austin <awoldes@gmail.com>
2024-01-21 16:15:02 +1000

    Update app/models/tour.rb

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,7 +2,6 @@ class Tour < ApplicationRecord
   has_many :expenses, dependent: :destroy
 
   def total_cost
-    ap self.expenses
     self.expenses.where.not(cost: nil).map(&:cost)&.sum
   end
 end
